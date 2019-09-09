671552b8d3097e1e816013c89c6ea81ba3f26e92
Alex Kotlar <akotlar@bu.edu>
2019-09-08 22:45:28 -0400

    fixes

--- vcf2/src/main.rs
+++ vcf2/src/main.rs
@@ -10,9 +10,9 @@ use crossbeam_utils::thread as cthread;
 use atoi::FromRadix10;
 use itoa;
 
+use lz4::Decoder;
 use memchr::memchr;
 use num_cpus;
-use regex::Regex;
 
 extern crate log;
 
@@ -1102,18 +1102,12 @@ impl<'a> Header<'a> {
     }
 }
 
-use lz4::Decoder;
-
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
-    let file_name = &args[1];
 
-    let extension = file_name.rsplit('.').nth(0).unwrap();
-    eprintln!("EXTENSION {}", extension);
     let input_file = std::fs::File::open(&args[1])?;
     let decoder = Decoder::new(input_file)?;
-    let mut reader = std::io::BufReader::with_capacity(48 * 1024 * 1024, decoder);
+    let mut reader = std::io::BufReader::with_capacity(16 * 1024 * 1024, decoder);
 
     let (head, n_eol_chars) = get_header_and_num_eol_chars(&mut reader);
     let header = Header::new(&head, true);
@@ -1127,7 +1121,7 @@ fn main() -> Result<(), io::Error> {
             let max_lines = 48;
             let mut len;
             let mut lines: Vec<Vec<u8>> = Vec::with_capacity(max_lines);
-            let mut buf = Vec::with_capacity(1 * 1024 * 1024);
+            let mut buf = Vec::with_capacity(16 * 1024 * 1024);
 
             loop {
                 // https://stackoverflow.com/questions/43028653/rust-file-i-o-is-very-slow-compared-with-c-is-something-wrong
