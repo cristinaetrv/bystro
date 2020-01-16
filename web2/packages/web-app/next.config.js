b3b3cdd3b75f65a9e4863267cd18e07793b1a6a9
Alex Kotlar <akotlar@bu.edu>
2020-01-16 00:15:33 -0500

    update deps

--- web2/packages/web-app/next.config.js
+++ web2/packages/web-app/next.config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const withPurgeCss = require('next-purgecss');
 const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
-const withNextSize = require('next-size');
 
 function disableCacheDirectory(config) {
   config.module.rules
@@ -26,25 +25,13 @@ const nextConfig = {
     keyframes: true,
     fontFace: true
   },
-  // Specify which files should be checked for selectors before deciding some imported css is not used:
   purgeCssPaths: ['pages/**/*', 'components/**/*'],
-  webpack: (config, options) => {
-    if (options.isServer) {
-      //
-    } else if (!options.dev) {
-      // Only in a production environment, in the client-side webpack phase
-      // does next configure splitChuunks
-      // In practice this happens during "next build", and we don't care about
-      // optimizing chunk settings during dev
-      config.optimization.splitChunks.cacheGroups.commons.minChunks = 2;
-    }
-
+  webpack: (config) => {
     disableCacheDirectory(config);
 
-    config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
   publicRuntimeConfig
 };
 
-module.exports = withNextSize(withSass(withCss(withPurgeCss(nextConfig))));
+module.exports = withSass(withCss(withPurgeCss(nextConfig)));
--- web2/packages/web-app/pages/_app.tsx
+++ web2/packages/web-app/pages/_app.tsx
@@ -1,64 +1,20 @@
 import App from "next/app";
 import Header from "../components/Header/Header";
 import { initIdTokenHandler } from "../libs/auth";
-import "animate.css";
-import "normalize.css";
-import "styles/main.scss";
 
 import { init as initJobTracker } from "../libs/jobTracker/jobTracker";
 import { addAuthCallbacks as socketIOAuthCallbacks } from "../libs/socketio";
 
-// const isServer = typeof window === "undefined";
-
-// let NodeCookies: any;
-// if (isServer) {
-//   NodeCookies = require("cookies");
-// }
+import "animate.css";
+import "normalize.css";
+import "styles/main.scss";
 
 declare type props = {
   Component: any;
   ctx: any;
 };
-// let authInitialized = false;
-// TODO: think about using React context to pass down auth state instead of prop
+
 export default class MyApp extends App<props> {
-  // This runs:
-  // 1) SSR Mode: One time
-  // 2) Client mode: After constructor (on client-side route transitions)
-
-  // static async getInitialProps({ Component, ctx }: props) {
-  //   let pageProps: any = {};
-
-  //   if (isServer) {
-  //     // Run here because we have no access to ctx in constructor
-  //     // initStateSSR(ctx.req.headers.cookie);
-  //   }
-
-  //   if (protectedRoute[ctx.pathname] === true) {
-  //     // ctx only exists only on server
-  //     if (ctx.res) {
-  //       const cookies = new NodeCookies(ctx.req, ctx.res);
-  //       cookies.set("referrer", ctx.pathname);
-
-  //       ctx.res.writeHead(303, { Location: "/login?redirect=true" });
-  //       ctx.res.end();
-
-  //       return { pageProps };
-  //     }
-
-  //     jscookies.set("referrer", ctx.pathname);
-  //     Router.replace(`/login?redirect=true`);
-
-  //     return { pageProps };
-  //   }
-
-  //   if (Component.getInitialProps) {
-  //     pageProps = await Component.getInitialProps(ctx);
-  //   }
-
-  //   return { pageProps };
-  // }
-
   componentDidMount() {
     initJobTracker();
     socketIOAuthCallbacks();
@@ -73,7 +29,6 @@ export default class MyApp extends App<props> {
         <Header />
         <span id="main">
           <Component {...pageProps} />
-          {/* <ACard /> */}
         </span>
       </span>
     );
